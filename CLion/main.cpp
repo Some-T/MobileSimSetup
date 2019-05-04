f0328221886c5e2b61f53d8026dbfe73ca185726
Jamie Cropley <somet@protonmail.com>
2019-05-04 23:50:54 +0100

    Added Aria base files back.

--- CLion/main.cpp
+++ CLion/main.cpp
@@ -1,6 +1,70 @@
-#include <iostream>
+#include "Aria.h"
+#include "forward.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv)
+{
+	Aria::init();
+	ArArgumentParser argParser(&argc, argv);
+	argParser.loadDefaultArguments();
+	ArRobot robot;
+	ArRobotConnector robotConnector(&argParser, &robot);
+	ArLaserConnector laserConnector(&argParser, &robot, &robotConnector);
+
+	// Always try to connect to the first laser:
+	argParser.addDefaultArgument("-connectLaser");
+
+	if (!robotConnector.connectRobot())
+	{
+		ArLog::log(ArLog::Terse, "Could not connect to the robot.");
+		if (argParser.checkHelpAndWarnUnparsed())
+		{
+			// -help not given, just exit.
+			Aria::logOptions();
+			Aria::exit(1);
+		}
+	}
+
+
+	// Trigger argument parsing
+	if (!Aria::parseArgs() || !argParser.checkHelpAndWarnUnparsed())
+	{
+		Aria::logOptions();
+		Aria::exit(1);
+	}
+
+	ArKeyHandler keyHandler;
+	Aria::setKeyHandler(&keyHandler);
+	robot.attachKeyHandler(&keyHandler);
+
+	puts("Press  Escape to exit.");
+
+	ArSonarDevice sonar;
+	robot.addRangeDevice(&sonar);
+
+	robot.runAsync(true);
+
+
+	// try to connect to laser. if fail, warn but continue, using sonar only
+	if (!laserConnector.connectLasers())
+	{
+		ArLog::log(ArLog::Normal, "Warning: unable to connect to requested lasers, will wander using robot sonar only.");
+	}
+
+
+	// turn on the motors
+	robot.enableMotors();
+
+
+	// add a set of actions that combine together to effect the wander behavior
+	ArActionStallRecover recover;
+	ArActionBumpers bumpers;
+	forward forward;
+	robot.addAction(&recover, 100);
+	robot.addAction(&bumpers, 75);
+	robot.addAction(&forward, 50);
+
+	// wait for robot task loop to end before exiting the program
+	robot.waitForRunExit();
+
+	Aria::exit(0);
 }
